8aaf5fa786d5f9ae7e81f71ae9f5b999d431e56f
Sumeet S Adhav <65108927+sumeet0805@users.noreply.github.com>
2024-02-04 15:27:11 +0530

    Update users.js

--- src/apicalls/users.js
+++ src/apicalls/users.js
@@ -4,7 +4,7 @@ import { axiosInstance } from ".";
 export const LoginUser = async (user) => {
   try {
     const response = await axiosInstance.post(
-      "http://localhost:5000/api/users/login",
+      "https://businessstationservice.onrender.com/api/users/login",
       user
     );
     return response.data;
@@ -15,7 +15,7 @@ export const LoginUser = async (user) => {
 
 export const RegisterUser = async (user) => {
   try {
-    const response=await axiosInstance.post("http://localhost:5000/api/users/register",user);
+    const response=await axiosInstance.post("https://businessstationservice.onrender.com/api/users/register",user);
     return response.data;
 
   } catch (err) {
@@ -25,7 +25,7 @@ export const RegisterUser = async (user) => {
 
 export const currentUser = async () => {
   try{
-    const response= await axiosInstance.get("http://localhost:5000/api/users/currentuser");
+    const response= await axiosInstance.get("https://businessstationservice.onrender.com/api/users/currentuser");
     return response.data;
   }catch(error){
     return error.response.data;
@@ -34,7 +34,7 @@ export const currentUser = async () => {
 
 export const getAllUsers = async () =>{
   try{
-    const response= await axiosInstance.get("http://localhost:5000/api/users/get-all-users");
+    const response= await axiosInstance.get("https://businessstationservice.onrender.com/api/users/get-all-users");
     return response.data;
   }catch(err){
     return err.response.data;
